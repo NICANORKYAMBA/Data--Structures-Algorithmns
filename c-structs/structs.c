b447f5ed3208ed5372d7f200b83aacf5d7a78131
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-11-24 11:59:14 +0300

    Updating the struct variables

--- c-structs/structs.c
+++ c-structs/structs.c
@@ -13,6 +13,8 @@ typedef struct car {
 	int speed;
 } car;
 
+void update_car(car *c, char *newname, float newprice, int newspeed);
+
 /**
  * main - entry point
  *
@@ -30,5 +32,27 @@ int main(void)
 	printf("Price: %f\n", saturn.price);
 	printf("Speed: %d\n", saturn.speed);
 
+	update_car(&saturn, "Toyota Model",1990.89, 450);
+
+	printf("\nNew Model: %s\n", saturn.name);
+	printf("New Price: %f\n", saturn.price);
+	printf("New Speed: %d\n", saturn.speed);
+
 	return (0);
+}
+
+/**
+ * update_car - function to update the car
+ *
+ * @newname: new name to the car model
+ * @newprice: price of the new model
+ * @newspeed: new speed of the updated car
+ *
+ * Return: (void)
+ */
+void update_car(car *c, char *newname, float newprice, int newspeed)
+{
+	c->name = newname;
+	c->price = newprice;
+	c->speed = newspeed;
 }
